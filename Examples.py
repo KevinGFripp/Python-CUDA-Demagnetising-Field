54550f22271ad9d6ecffda9fab334e37f73a4060
Kevin Fripp <96999054+KevinGFripp@users.noreply.github.com>
2026-03-24 23:25:54 +0000

    Add files via upload

---
+++ Examples.py
@@ -0,0 +1,31 @@
+from Example_ComputeDipoleField import example_compute_dipole_field
+from Example_ComputeDemagField import example_compute_demag_field
+
+if __name__ == "__main__":
+
+    example_compute_dipole_field()
+    example_compute_demag_field()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
